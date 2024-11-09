7d5c02373a6124df44a1603856bf61d7163ffa8e
Ultrasolutions2024 <ghultraflysolutions@gmail.com>
2024-11-09 15:49:16 +0530

    Update constant.js

--- gravity/src/constant.js
+++ gravity/src/constant.js
@@ -1,7 +1,7 @@
 // export const BASE_URL="https://gravity-global-exports-do75.onrender.com"
-// export const BASE_URL="http://gravityglobalexport.com"
-export const BASE_URL= window.location.protocol === 'https:' 
-    ? 'https://gravityglobalexport.com/product/get_all_product' 
-    : 'http://gravityglobalexport.com/product/get_all_product';
+export const BASE_URL="http://gravityglobalexport.com"
+// export const BASE_URL= window.location.protocol === 'https:' 
+//     ? 'https://gravityglobalexport.com/product/get_all_product' 
+//     : 'http://gravityglobalexport.com/product/get_all_product';
 
 // export const BASE_URL="localhost:8000"
