1c23676812044e1348e76443251df2e4a2c1e7af
Ultrasolutions2024 <ghultraflysolutions@gmail.com>
2024-11-09 15:26:16 +0530

    Update constant.js
    Update constant.js

--- gravity/src/constant.js
+++ gravity/src/constant.js
@@ -1,2 +1,3 @@
-export const BASE_URL="https://gravity-global-exports-do75.onrender.com"
+// export const BASE_URL="https://gravity-global-exports-do75.onrender.com"
+export const BASE_URL="http://gravityglobalexport.com"
 // export const BASE_URL="localhost:8000"
