971078e92baaa670a3ae4d3dabf22aab4e20f6ad
Ultrasolutions2024 <ghultraflysolutions@gmail.com>
2024-11-09 15:41:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ require("dotenv").config()
 const userRoute = require('./routes/adminRoutes')
 const productRoute = require('./routes/productRoutes')
 const connectDb = require('./config/db')
+res.setHeader('Access-Control-Allow-Origin', '*');  // Or specify your frontend domain
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 const app = express()
 const PORT = process.env.PORT || 8000
